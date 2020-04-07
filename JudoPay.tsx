e357de232e9d252c7b91a47a8be760b7c3c7d1a7
Mihai <mihai.petrenco@endava.com>
2020-04-07 15:40:35 +0300

    Updated the native iOS wrapper

---
+++ JudoPay.tsx
@@ -0,0 +1,49 @@
+import { NativeModules } from 'react-native';
+import { JudoTransactionType, JudoConfiguration, JudoResponse } from './types/JudoTypes';
+
+class JudoPay {
+
+    public isSandboxed: boolean = true;
+
+    private token: string;
+    private secret: string;
+
+    constructor(token: string, secret: string) {
+        this.token = token;
+        this.secret = secret;
+    }
+
+    public invokeTransaction(
+        type: JudoTransactionType,
+        configuration: JudoConfiguration
+    ): Promise<JudoResponse | null> {
+
+        const params = this.generateParameters(type, configuration);
+
+        return new Promise((resolve, reject) => {
+            const judoPay = NativeModules.RNJudo;
+            judoPay.invokeTransaction(params)
+                .then((response: any) => {
+                    resolve(response);
+                })
+                .catch((error: any) => {
+                    reject(error);
+                })
+        });
+    }
+
+    private generateParameters(
+        type: JudoTransactionType,
+        configuration: JudoConfiguration
+    ): any {
+        return {
+            'token': this.token,
+            'secret': this.secret,
+            'sandboxed': this.isSandboxed,
+            'type': type,
+            'configuration': configuration,
+        }
+    }
+}
+
+export default JudoPay;
