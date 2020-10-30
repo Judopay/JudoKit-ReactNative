28988fbf740c483212a17f72c68958fe48c6376f
Mihai Petrenco <mihai.petrenco@endava.com>
2020-10-30 13:23:24 +0200

    Updated RN tests

--- __tests__/JudoPayTest.tsx
+++ __tests__/JudoPayTest.tsx
@@ -1,14 +1,8 @@
 import 'react-native'
 
 import configuration from './JudoTestDefaults'
-
-import JudoPay from '../JudoPay'
-
-import {
-    JudoTransactionType,
-    JudoTransactionMode,
-    JudoAuthorization
-} from '../types/JudoTypes'
+import JudoPay from "../JudoPay";
+import {JudoAuthorization, JudoTransactionMode, JudoTransactionType} from '../types/JudoTypes'
 
 test('JudoPay after init has isSandboxed = true', () => {
     const authorization: JudoAuthorization = {
@@ -19,6 +13,17 @@ test('JudoPay after init has isSandboxed = true', () => {
     const judoPay = new JudoPay(authorization)
 
     expect(judoPay.isSandboxed).toBe(true)
+})
+
+test('isBankingAppAvailable will always return false on an emulator', async() => {
+    const authorization: JudoAuthorization = {
+        token: 'token',
+        paymentSession: 'paymentSession'
+    }
+
+    const judoPay = new JudoPay(authorization)
+    const response = await judoPay.isBankingAppAvailable()
+    expect(response).toBeFalsy();
 })
 
 test('invokeTransaction will return mocked JudoResponse', async () => {
@@ -142,6 +147,60 @@ test('invokePayByBankApp will return mocked JudoResponse', async () => {
     const judoPay = new JudoPay(authorization)
 
     const data = await judoPay.invokePayByBankApp(configuration)
+
+    expect(data.amount).toEqual('1000.0')
+    expect(data.appearsOnStatementAs).toEqual('nothing')
+    expect(data.createdAt).toEqual('12345678')
+    expect(data.merchantName).toEqual('merchantName')
+    expect(data.receiptId).toEqual('receiptId')
+    expect(data.yourPaymentReference).toEqual('paymentRef')
+    expect(data.type).toEqual('payment')
+    expect(data.result).toEqual('response_result')
+    expect(data.originalAmount).toEqual('1100.0')
+    expect(data.netAmount).toEqual('1100.0')
+    expect(data.currency).toEqual('USD')
+
+    expect.assertions(11)
+})
+
+test('fetchTransactionDetails will return mocked JudoResponse', async () => {
+    const authorization: JudoAuthorization = {
+        token: 'token',
+        secret: 'secret'
+    }
+
+    const judoPay = new JudoPay(authorization)
+
+    const data = await judoPay.fetchTransactionDetails('sample-receipt-id')
+
+    expect(data.amount).toEqual('1000.0')
+    expect(data.appearsOnStatementAs).toEqual('nothing')
+    expect(data.createdAt).toEqual('12345678')
+    expect(data.merchantName).toEqual('merchantName')
+    expect(data.receiptId).toEqual('receiptId')
+    expect(data.yourPaymentReference).toEqual('paymentRef')
+    expect(data.type).toEqual('payment')
+    expect(data.result).toEqual('response_result')
+    expect(data.originalAmount).toEqual('1100.0')
+    expect(data.netAmount).toEqual('1100.0')
+    expect(data.currency).toEqual('USD')
+
+    expect.assertions(11)
+})
+
+test('performTokenTransaction will return mocked JudoResponse', async () => {
+    const authorization: JudoAuthorization = {
+        token: 'token',
+        secret: 'secret'
+    }
+
+    const judoPay = new JudoPay(authorization)
+
+    const data = await judoPay.performTokenTransaction(
+        JudoTransactionMode.Payment,
+        configuration,
+        'sample-token'
+    )
 
     expect(data.amount).toEqual('1000.0')
     expect(data.appearsOnStatementAs).toEqual('nothing')
