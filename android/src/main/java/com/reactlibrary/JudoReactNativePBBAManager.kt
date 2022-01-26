82b459ef84512498ae859097cb40146404482ee8
Stefan Popa <stefan.popa@judopayments.com>
2022-01-26 12:13:19 +0200

    Android native wrapper using judokit version 2.1.9

--- android/src/main/java/com/reactlibrary/JudoReactNativePBBAManager.kt
+++ android/src/main/java/com/reactlibrary/JudoReactNativePBBAManager.kt
@@ -1,17 +1,17 @@
 package com.reactlibrary
 
+import android.widget.Button
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
-import com.judopay.judokit.android.ui.common.PayByBankButton
 
-class JudoReactNativePBBAManager: SimpleViewManager<PayByBankButton>() {
+class JudoReactNativePBBAManager: SimpleViewManager<Button>() {
 
     override fun getName(): String {
         return "RNPBBAButton"
     }
 
-    override fun createViewInstance(reactContext: ThemedReactContext): PayByBankButton {
-        return PayByBankButton(reactContext)
+    override fun createViewInstance(reactContext: ThemedReactContext): Button {
+        return Button(reactContext)
     }
 
 }
