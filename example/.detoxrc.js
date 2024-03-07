fb9c9d4fdda2ac0e404494fa4b39deb6c23823b9
Stefan Popa <popa.stefan.tudor@gmail.com>
2024-03-07 18:01:22 +0200

    chore(sdk): formatting files, runs only the sdk tests when `yarn test`

--- example/.detoxrc.js
+++ example/.detoxrc.js
@@ -2,82 +2,86 @@
 module.exports = {
   testRunner: {
     args: {
-      '$0': 'jest',
-      config: 'e2e/jest.config.js'
+      $0: 'jest',
+      config: 'e2e/jest.config.js',
     },
     jest: {
-      setupTimeout: 120000
-    }
+      setupTimeout: 120000,
+    },
   },
   apps: {
     'ios.debug': {
       type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/JudoKitReactNativeExample.app',
-      build: 'xcodebuild -workspace ios/JudoKitReactNativeExample.xcworkspace -configuration Debug -scheme JudoKitReactNativeExample -sdk iphonesimulator -derivedDataPath ios/build -quiet'
+      binaryPath:
+        'ios/build/Build/Products/Debug-iphonesimulator/JudoKitReactNativeExample.app',
+      build:
+        'xcodebuild -workspace ios/JudoKitReactNativeExample.xcworkspace -configuration Debug -scheme JudoKitReactNativeExample -sdk iphonesimulator -derivedDataPath ios/build -quiet',
     },
     'ios.release': {
       type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/JudoKitReactNativeExample.app',
-      build: 'xcodebuild -workspace ios/JudoKitReactNativeExample.xcworkspace -configuration Release -scheme JudoKitReactNativeExample -sdk iphonesimulator -derivedDataPath ios/build -quiet'
+      binaryPath:
+        'ios/build/Build/Products/Release-iphonesimulator/JudoKitReactNativeExample.app',
+      build:
+        'xcodebuild -workspace ios/JudoKitReactNativeExample.xcworkspace -configuration Release -scheme JudoKitReactNativeExample -sdk iphonesimulator -derivedDataPath ios/build -quiet',
     },
     'android.debug': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
-      build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
-      reversePorts: [
-        8081
-      ]
+      build:
+        'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
+      reversePorts: [8081],
     },
     'android.release': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
-      build: 'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release'
-    }
+      build:
+        'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release',
+    },
   },
   devices: {
     simulator: {
       type: 'ios.simulator',
       device: {
-        type: 'iPhone 15'
-      }
+        type: 'iPhone 15',
+      },
     },
     attached: {
       type: 'android.attached',
       device: {
-        adbName: '.*'
-      }
+        adbName: '.*',
+      },
     },
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_6a_API_34'
-      }
-    }
+        avdName: 'Pixel_6a_API_34',
+      },
+    },
   },
   configurations: {
     'ios.sim.debug': {
       device: 'simulator',
-      app: 'ios.debug'
+      app: 'ios.debug',
     },
     'ios.sim.release': {
       device: 'simulator',
-      app: 'ios.release'
+      app: 'ios.release',
     },
     'android.att.debug': {
       device: 'attached',
-      app: 'android.debug'
+      app: 'android.debug',
     },
     'android.att.release': {
       device: 'attached',
-      app: 'android.release'
+      app: 'android.release',
     },
     'android.emu.debug': {
       device: 'emulator',
-      app: 'android.debug'
+      app: 'android.debug',
     },
     'android.emu.release': {
       device: 'emulator',
-      app: 'android.release'
-    }
-  }
+      app: 'android.release',
+    },
+  },
 };
