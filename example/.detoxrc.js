1107c7906c045bbb617ec41045e2407c215bcb88
Eugene Zhernakov <eugene.zhernakov@gmail.com>
2024-09-16 15:11:43 +0100

    fix(test): update config.yml

--- example/.detoxrc.js
+++ example/.detoxrc.js
@@ -54,7 +54,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_32_x86_64',
+        avdName: 'Pixel_4_API_32_x86_64',
       },
     },
   },
