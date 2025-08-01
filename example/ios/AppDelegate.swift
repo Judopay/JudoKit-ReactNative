c282dc1fefaaef481755dc6759cad846e9864c53
Rafal Ozog <rafal.ozog@judopay.com>
2025-08-01 08:52:51 +0100

    chore(deps): fixes wormholy

--- example/ios/AppDelegate.swift
+++ example/ios/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import Wormholy
 
 @main
 class AppDelegate: RCTAppDelegate {
@@ -9,6 +10,7 @@ class AppDelegate: RCTAppDelegate {
     self.moduleName = "JudoKitReactNativeExample"
     self.dependencyProvider = RCTAppDependencyProvider()
     self.initialProps = [:]
+    WormholySetup.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
   
@@ -22,5 +24,28 @@ class AppDelegate: RCTAppDelegate {
 #else
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
+  }
+}
+
+@objc class WormholySetup: NSObject {
+  @objc static func configure() {
+    // 'SwiftyLoad' is necessary to initialize the Wormholy (otherwise doesn't display its UI on shake).
+    Wormholy.swiftyLoad()
+    // This step swaps impl. of URLSessionConfiguration.default (otherwise API requests are not displayed in Wormholy).
+    let originalSelector = #selector(getter: URLSessionConfiguration.default)
+    let swizzledSelector = #selector(URLSessionConfiguration.wormholy_default)
+    let originalMethod = class_getClassMethod(URLSessionConfiguration.self, originalSelector)
+    let swizzledMethod = class_getClassMethod(URLSessionConfiguration.self, swizzledSelector)
+    if let originalMethod = originalMethod, let swizzledMethod = swizzledMethod {
+        method_exchangeImplementations(originalMethod, swizzledMethod)
+    }
+  }
+}
+
+extension URLSessionConfiguration {
+  @objc class func wormholy_default() -> URLSessionConfiguration {
+    let config = wormholy_default()
+    Wormholy.enable(true, sessionConfiguration: config)
+    return config
   }
 }
