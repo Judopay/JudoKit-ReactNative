e4f083363521d0ab4c5150afb0eb48a2b4eef657
Stefan Popa <popa.stefan.tudor@gmail.com>
2025-08-08 00:34:09 +0300

    fix(demo): move away from the deprecated RCTAppDelegate

--- example/ios/AppDelegate.swift
+++ example/ios/AppDelegate.swift
@@ -5,19 +5,43 @@ import ReactAppDependencyProvider
 import Wormholy
 
 @main
-class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    self.moduleName = "JudoKitReactNativeExample"
-    self.dependencyProvider = RCTAppDependencyProvider()
-    self.initialProps = [:]
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+
+  var reactNativeDelegate: ReactNativeDelegate?
+  var reactNativeFactory: RCTReactNativeFactory?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    let delegate = ReactNativeDelegate()
+    let factory = RCTReactNativeFactory(delegate: delegate)
+    delegate.dependencyProvider = RCTAppDependencyProvider()
+
+    reactNativeDelegate = delegate
+    reactNativeFactory = factory
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+
+    factory.startReactNative(
+      withModuleName: "JudoKitReactNativeExample",
+      in: window,
+      launchOptions: launchOptions
+    )
+
     WormholySetup.configure()
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    return true
   }
   
+}
+
+class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
     self.bundleURL()
   }
-  
+
   override func bundleURL() -> URL? {
 #if DEBUG
     RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
